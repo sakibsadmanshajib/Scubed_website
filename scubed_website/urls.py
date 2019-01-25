3c899671fb4e65e98a67e4f105091094389451f8
Sakib Sadman Shajib <sakibsadmanshajib@gmail.com>
2019-01-25 08:03:48 +0600

    FinMan - S Cubed
    - User added

--- scubed_website/urls.py
+++ scubed_website/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+
+    path('', include('base.urls')),
+    path('finman/', include('finance_management.urls')),
+
 ]
